147c6ec9f6827c3c8465d08eae4759bd80ca2fcc
Guilherme Guimarães <guima_dev@hotmail.com>
2018-12-14 00:52:44 -0200

    Projeto iniciado mostrando a mensagem HelloWord!

---
+++ secB_git_basico_comando_init/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace secB_git_basico_comando_init
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
