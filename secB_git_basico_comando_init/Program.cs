8a5895dcd130a060ca1e95b393dcf92dfa2b1607
Guilherme Guimarães <guima_dev@hotmail.com>
2018-12-14 01:27:07 -0200

    Loop infinito adicionado ao projeto

--- secB_git_basico_comando_init/Program.cs
+++ secB_git_basico_comando_init/Program.cs
@@ -10,6 +10,10 @@ namespace secB_git_basico_comando_init
             Console.WriteLine("Modificado para versionamento!");
             Console.WriteLine("Mais uma modificação.");
             Console.WriteLine("E outra!");
+            for(int i = 0; i >= 0; i++)
+            {
+                Console.WriteLine("LOOP INFINITO");
+            }
             Console.ReadLine();
         }
     }
